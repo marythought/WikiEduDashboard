3c597e0988965c33cd1fbbd00f47097a87455ba1
Sage Ross <ragesoss@gmail.com>
2016-02-18 12:53:29 -0800

    Make the static instance methods of course type accessible
    This way, we can also call these methods even outside the context of an existing course, without creating a new one.

--- app/models/classroom_program_course.rb
+++ app/models/classroom_program_course.rb
@@ -1,15 +1,24 @@
 class ClassroomProgramCourse < Course
-  def wiki_edits_enabled?
-    true
+  # These methods are available as both class and instance methods
+  module ClassroomProgramCourseProperties
+    def wiki_edits_enabled?
+      true
+    end
+
+    def string_prefix
+      'courses'
+    end
   end
 
+  extend ClassroomProgramCourseProperties
+  include ClassroomProgramCourseProperties
+
+  ####################
+  # Instance methods #
+  ####################
   def wiki_title
     prefix = ENV['course_prefix'] + '/'
     escaped_slug = slug.tr(' ', '_')
     "#{prefix}#{escaped_slug}"
-  end
-
-  def string_prefix
-    'courses'
   end
 end
